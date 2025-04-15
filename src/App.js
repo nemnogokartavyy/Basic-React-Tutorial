93d90c6fb69e5f15f114abc6a52a06e666bd49ab
nemnogokartavyy <iikorzun@yandex.by>
2025-04-15 17:38:24 +0300

    React Prime 10/112 chapter 1 task 6 gtask

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
-// ⊗jsrtPmJxRD
+// ⊗jsrtPmJxRS
 
 import React from 'react';
 
 {/* #1 */ }
 function App() {
   return (
-    <ul>
-      <li>text1</li>
-      <li>text2</li>
-      <li>text3</li>
-    </ul>
+    <>
+      <ul>
+        <li>text1</li>
+        <li>text2</li>
+        <li>text3</li>
+      </ul>
+      <ul>
+        <li>text1</li>
+        <li>text2</li>
+        <li>text3</li>
+      </ul>;
+    </>
   );
 }
 
