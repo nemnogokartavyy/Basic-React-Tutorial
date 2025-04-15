b860781ae5cbacd0c01019dd5839cd4b7b5289a9
nemnogokartavyy <iikorzun@yandex.by>
2025-04-15 17:47:50 +0300

    React Prime 14/112 chapter 1 task 10 gtask

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-// ⊗jsrtPmJxVI
+// ⊗jsrtPmJxVN
 
 import React from 'react';
 
 {/* #1 */ }
 function App() {
-	const str1 = 'text1';
-	const str2 = 'text1';
-	
-	return <div>
-		<p>{str1}</p>
-		<p>{str2}</p>
-	</div>;
+  const name = 'user';
+  const age = '30';
+
+  return <div>
+    name: {name},
+    age:  {age}
+  </div>;
 }
 
 export default App;
