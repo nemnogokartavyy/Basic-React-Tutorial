97595300afc9b7fcae5b443dd687e5eedeb4f1f3
nemnogokartavyy <iikorzun@yandex.by>
2025-04-15 20:29:11 +0300

    React Prime 34/112 chapter 1 task 27 gtask

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
-// ⊗jsrtPmFcHd
+// ⊗jsrtPmFcHP
 
 import React from 'react';
 
 {/* #1 */ }
 function App() {
-  function show1() {
-    alert(1);
+  function showMess(name) {
+    alert(name);
   }
-
-  function show2() {
-    alert(2);
-  }
-
   return <div>
-    <button onClick={show1}>act1</button>
-    <button onClick={show2}>act2</button>
+    <button onClick={() => showMess(1)}>act1</button>
+    <button onClick={() => showMess(2)}>act2</button>
+    <button onClick={() => showMess(3)}>act3</button>
   </div>;
 }
 
