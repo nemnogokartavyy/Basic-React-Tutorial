11276f3ec959209149d51c5ae62edfe02f799f50
nemnogokartavyy <iikorzun@yandex.by>
2025-04-15 19:43:16 +0300

    React Prime 26/112 chapter 1 task 19 gtask

--- src/App.js
+++ src/App.js
@@ -1,64 +1,22 @@
-// ⊗jsrtPmJxTCr
+// ⊗jsrtPmCdSh
 
 import React from 'react';
 
+{/* #1 */ }
 function App() {
+  let isAdult;
+  let text;
+  let age = 20;
 
-  {/* #1 */ }
-  const list = (
-    <ul>
-      <li>1</li>
-      <li>2</li>
-      <li>3</li>
-      <li>4</li>
-      <li>5</li>
-    </ul>
-  );
+  if (age >= 18) {
+    isAdult = true;
+  } else {
+    isAdult = false;
+  }
 
-  {/* #2 */ }
-  const table = (
-    <table>
-      <thead>
-        <tr>
-          <th>Ячейка шапки 1</th>
-          <th>Ячейка шапки 2</th>
-          <th>Ячейка шапки 3</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>1-1</td>
-          <td>1-2</td>
-          <td>1-3</td>
-        </tr>
-        <tr>
-          <td>2-1</td>
-          <td>2-2</td>
-          <td>2-3</td>
-        </tr>
-        <tr>
-          <td>3-1</td>
-          <td>3-2</td>
-          <td>3-3</td>
-        </tr>
-      </tbody>
-      <tfoot>
-        <tr>
-          <td>Ячейка подвала 1</td>
-          <td>Ячейка подвала 2</td>
-          <td>Ячейка подвала 3</td>
-        </tr>
-      </tfoot>
-    </table>
-  )
+  isAdult ? text = <p>Есть 18 лет</p> : text = <p>Нет 18 лет</p>;
 
-  return (
-    <>
-      {list}
-      {table}
-    </>
-  );
-
+  return text;
 }
 
 export default App;
