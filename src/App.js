d10fe5331be1e6fd1bb5e9ce8921551b0383b9a7
nemnogokartavyy <iikorzun@yandex.by>
2025-04-15 20:45:47 +0300

    React Prime 36/112 chapter 2-4 tasks 30-32 gtasks

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
-// ⊗jsrtPmFcEO
+// ⊗jsrtPmFcEOP
 
 import React from 'react';
 
 function App() {
 
   //1
-  function func1(event) {
-    console.log(event);
+  function func1(arg1, arg2, event) {
+    console.log(arg1, arg2, event);
   }
 
   //2
-  function func2(event) {
-    console.log(event.target);
+  function func2(event, arg1, arg2) {
+    console.log(event, arg1, arg2);
+  }
+
+  //3
+  function func3(arg1, event, arg2) {
+    console.log(arg1, event, arg2);
   }
 
   return <div>
-    <button onClick={func1}>act1</button>
-    <button onClick={func2}>act2</button>
+    <button onClick={event => func1('eee', 'aaa', event)}>act</button>
+    <button onClick={event => func2(event, 'eee', 'aaa')}>act</button>
+    <button onClick={event => func3('eee', event, 'aaa')}>act</button>
   </div>;
 
 }
