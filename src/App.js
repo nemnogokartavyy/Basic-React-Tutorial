8a866736062784ae8b4407251eb7c12dba39af73
nemnogokartavyy <iikorzun@yandex.by>
2025-04-15 21:49:25 +0300

    React Prime 47-48/112 chapters 40-42 gtasks

--- src/App.js
+++ src/App.js
@@ -1,135 +1,37 @@
 import React from 'react';
+import uuid from 'react-uuid';
+import { nanoid } from 'nanoid'
 
+// ⊗jsrtPmIdGn
+//1
 function App() {
 
-  // ⊗jsrtPmFmLI
+  let arr = [1, 2, 3, 4, 5];
+
+  let res1 = arr.map(elem => {
+    return <li key={uuid()} id={uuid()}>{elem}</li>
+  });
+
+  let res2 = arr.map(elem => {
+    return <li key={nanoid()} id={nanoid()}>{elem}</li>
+  });
+
+  // ⊗jsrtPmIdFn
   //1
-  function func1() {
-    const arr = [
-      <li>1</li>,
-      <li>2</li>,
-      <li>3</li>,
-      <li>4</li>,
-      <li>5</li>,
-    ];
-    return <ul>
-      {arr}
-    </ul>
+  function nanoId() {
+    return nanoid();
   }
 
-  // ⊗jsrtPmFmLTA
-  //1
-  function func2() {
-    const arr = [];
-    for (let i = 0; i < 5; i++) {
-      arr.push(<li>{i + 1}</li>)
-    }
-    return <ul>
-      {arr}
-    </ul>
-  }
-
-  // ⊗jsrtPmFmTAD
-  //1
-  function func3() {
-    const arr = ['a', 'b', 'c', 'd', 'e'];
-    let result = arr.map(elem => {
-      return <li>{elem}</li>
-    })
-    return <ul>
-      {result}
-    </ul>
-  }
-
-  // ⊗jsrtPmFmLP
-  //1
-  function func4() {
-    const array = ['a', 'b', 'c', 'd', 'e'];
-    let result = array.map((elem, index) => {
-      return <li key={index}>{elem}</li>
-    })
-    return <ul>
-      {result}
-    </ul>
-  }
-
-  // ⊗jsrtPmFmAOO
-  //1
-  function func5() {
-    const users = [
-      { name: 'user1', surn: 'surn1', age: 30 },
-      { name: 'user2', surn: 'surn2', age: 31 },
-      { name: 'user3', surn: 'surn3', age: 32 },
-    ];
-    let result = users.map((elem, index) => {
-      return <li key={index}>
-        <span>{elem.name}</span>
-        <span>{elem.surn}</span>
-        <span>{elem.age}</span>
-      </li>
-    });
-    return <ul>
-      {result}
-    </ul>
-  }
-
-  // ⊗jsrtPmFmUKI
-  //1
-  function func5() {
-    const users = [
-      { id: 1, name: 'user1', surn: 'surn1', age: 30 },
-      { id: 2, name: 'user2', surn: 'surn2', age: 31 },
-      { id: 3, name: 'user3', surn: 'surn3', age: 32 },
-    ];
-    let result = users.map((elem) => {
-      return <li key={elem.id}>
-        <span>{elem.name} </span>
-        <span>{elem.surn} </span>
-        <span>{elem.age} </span>
-      </li>
-    });
-    return <ul>
-      {result}
-    </ul>
-  }
-
-  // ⊗jsrtPmFmTA
-  //1
-  function func6() {
-    const users = [
-      { id: 1, name: 'user1', surn: 'surn1', age: 30 },
-      { id: 2, name: 'user2', surn: 'surn2', age: 31 },
-      { id: 3, name: 'user3', surn: 'surn3', age: 32 },
-    ];
-    let result = users.map(user => {
-      return <tr>
-        <td>{user.name}</td>
-        <td>{user.surn}</td>
-        <td>{user.age}</td>
-      </tr>
-    });
-    return <table>
-      <thead>
-        <tr>
-          <th>имя</th>
-          <th>фамилия</th>
-          <th>возраст</th>
-        </tr>
-      </thead>
-      <tbody>
-        {result}
-      </tbody>
-    </table>
+  // ⊗jsrtPmIdFn
+  //2
+  function uuidId() {
+    return uuid();
   }
 
   return (
     <>
-      {func1()}
-      {func2()}
-      {func3()}
-      {func4()}
-      {func5()}
-      {func6()}
+      <ul>{res1}</ul>
+      <ul>{res2}</ul>
     </>
   )
 
