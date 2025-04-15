006c8f4d51da9811646de7286897009adf894d31
nemnogokartavyy <iikorzun@yandex.by>
2025-04-15 18:07:43 +0300

    React Prime 19/112 chapter 1 task 14 gtask

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-// ⊗jsrtPmJxVAt
+// ⊗jsrtPmJxTS
 
 import React from 'react';
 
 {/* #1 */ }
 function App() {
-  const attr = 'block';
+  const li1 = <li>text1</li>;
+  const li2 = <li>text2</li>;
+  const li3 = <li>text3</li>;
+  const list = <ul>{li1}{li2}{li3}</ul>;
 
-  return <div id={attr}>
-    text
-  </div>;
+  return list;
 }
 
 export default App;
