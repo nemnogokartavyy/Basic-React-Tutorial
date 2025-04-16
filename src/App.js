32ee18fc6034b1399c57e87eef7a34381c203bac
nemnogokartavyy <iikorzun@yandex.by>
2025-04-16 17:31:08 +0300

    React Prime 60/112 chapter 1 task 54 gtask

--- src/App.js
+++ src/App.js
@@ -1,30 +1,35 @@
-// ⊗jsrtPmFmsIS
+// ⊗jsrtPmFmsDt
 
 import React, { useState } from 'react';
 
 //1
 function App() {
 
-  const [input1, setInput1] = useState(1);
-  const [input2, setInput2] = useState(2);
-  const [input3, setInput3] = useState(3);
-  const [input4, setInput4] = useState(4);
-  const [input5, setInput5] = useState(5);
+  const [input1, setInput1] = useState(0);
+  const [input2, setInput2] = useState(0);
+  const [result, setResult] = useState(0);
 
-  function sum(num1, num2, num3, num4, num5) {
-    let arr = [num1, num2, num3, num4, num5];
-    let sum = arr.reduce((acc, elem) => acc += +elem, 0);
-    return sum / arr.length;
+
+  function sum(num1, num2) {
+    setResult(+num1 + +num2);
   }
+
+  function mul(num1, num2) {
+    setResult(num1 * num2);
+  }
+
 
   return (
     <>
       <input value={input1} onChange={(event) => { setInput1(event.target.value) }} />
       <input value={input2} onChange={(event) => { setInput2(event.target.value) }} />
-      <input value={input3} onChange={(event) => { setInput3(event.target.value) }} />
-      <input value={input4} onChange={(event) => { setInput4(event.target.value) }} />
-      <input value={input5} onChange={(event) => { setInput5(event.target.value) }} />
-      <p>Среднее арифметическое: {sum(input1, input2, input3, input4, input5)}</p>
+      
+      <button onClick={() => sum(input1, input2)}>ClickSum</button>
+      <button onClick={() => mul(input1, input2)}>ClickMul</button>
+
+      <p>
+        {result}
+      </p>
     </>
   )
 
