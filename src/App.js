280f156cb33597e1f1293665ee6d243d3132808f
nemnogokartavyy <iikorzun@yandex.by>
2025-04-16 18:02:27 +0300

    React Prime 60/112 chapter 4 task 57 gtask

--- src/App.js
+++ src/App.js
@@ -2,35 +2,25 @@
 
 import React, { useState } from 'react';
 
-//3
+//4
 function App() {
 
-  let dateNow = new Date();
-
-  const [input1, setInput1] = useState(formDate(dateNow));
-  const [input2, setInput2] = useState(formDate(dateNow));
+  const [input, setInput] = useState(0);
   const [result, setResult] = useState(0);
 
-  function func(dateInput1, dateInput2) {
-    let date1 = new Date(dateInput1);
-    let date2 = new Date(dateInput2);
-    setResult(Math.abs(date1 - date2) / (1000 * 60 * 60 * 24));
-  }
-
-  function formDate(date) {
-    let result = date.toLocaleString().slice(0, 10).split('.').reverse().join('-');
-    return result;
+  function sum(number) {
+    setResult(number.split('').reduce((acc, elem) => acc += + elem, 0));
   }
 
   return (
     <>
-      <input value={input1} onChange={(event) => { setInput1(event.target.value) }} />
-      <input value={input2} onChange={(event) => { setInput2(event.target.value) }} />
-
-      <button onClick={() => func(input1, input2)}>ClickSum</button>
+      <input type='number' value={input}
+        onChange={(event) => { setInput(event.target.value) }}
+        onBlur={() => sum(input)}
+      />
 
       <p>
-        Разница между датами в днях: {result}.
+        Сумма цифр числа: {result}.
       </p>
     </>
   )
