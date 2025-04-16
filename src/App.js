b2076779db2115d4edf30e7428dbd32028eec758
nemnogokartavyy <iikorzun@yandex.by>
2025-04-16 19:15:31 +0300

    React Prime 61/112 chapter 2 task 60 gtask

--- src/App.js
+++ src/App.js
@@ -2,43 +2,23 @@
 
 import React, { useState } from 'react';
 
-//1
+//2
 function App() {
 
-  const [value, setValue] = useState('');
+  const [value, setValue] = useState(`1
+2
+3
+4
+5`);
   const [res, setRes] = useState();
 
-  function translit(text) {
-    let answer = '';
-    let converter = {
-      'а': 'a', 'б': 'b', 'в': 'v', 'г': 'g', 'д': 'd',
-      'е': 'e', 'ё': 'e', 'ж': 'zh', 'з': 'z', 'и': 'i',
-      'й': 'y', 'к': 'k', 'л': 'l', 'м': 'm', 'н': 'n',
-      'о': 'o', 'п': 'p', 'р': 'r', 'с': 's', 'т': 't',
-      'у': 'u', 'ф': 'f', 'х': 'h', 'ц': 'c', 'ч': 'ch',
-      'ш': 'sh', 'щ': 'sch', 'ь': '', 'ы': 'y', 'ъ': '',
-      'э': 'e', 'ю': 'yu', 'я': 'ya',
-      'А': 'A', 'Б': 'B', 'В': 'V', 'Г': 'G', 'Д': 'D',
-      'Е': 'E', 'Ё': 'E', 'Ж': 'Zh', 'З': 'Z', 'И': 'I',
-      'Й': 'Y', 'К': 'K', 'Л': 'L', 'М': 'M', 'Н': 'N',
-      'О': 'O', 'П': 'P', 'Р': 'R', 'С': 'S', 'Т': 'T',
-      'У': 'U', 'Ф': 'F', 'Х': 'H', 'Ц': 'C', 'Ч': 'Ch',
-      'Ш': 'Sh', 'Щ': 'Sch', 'Ь': '', 'Ы': 'Y', 'Ъ': '',
-      'Э': 'E', 'Ю': 'Yu', 'Я': 'Ya'
-    };
-    for (let i = 0; i < text.length; i++) {
-      if (converter[text[i]] == undefined) {
-        answer += text[i];
-      } else {
-        answer += converter[text[i]];
-      }
-    }
-    return answer;
+  function func(value) {
+    setRes(value.split(/\s/).reduce((acc, elem) => acc += +elem, 0));
   }
 
   return (
     <>
-      <textarea value={value} onChange={(event) => { setRes(translit(event.target.value)); setValue(event.target.value); }} />
+      <textarea value={value} onChange={(event) => { setValue(event.target.value); func(event.target.value) }} />
 
       <p>
         {res}
