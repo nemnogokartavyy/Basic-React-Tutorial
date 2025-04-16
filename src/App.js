09c22f89d9fcd869211bf12782ea2fa1c2124b18
nemnogokartavyy <iikorzun@yandex.by>
2025-04-16 23:00:04 +0300

    React Prime 62/112 chapter 2 task 62 gtask

--- src/App.js
+++ src/App.js
@@ -2,20 +2,34 @@
 
 import React, { useState } from 'react';
 
-//1
+//2
 function App() {
 
-  const [checked, setChecked] = useState(false);
-  const [text, setText] = useState();
+  const [checked1, setChecked1] = useState(false);
+  const [checked2, setChecked2] = useState(false);
+  const [checked3, setChecked3] = useState(false);
 
   return (
     <>
-      <input type='checkbox' checked={checked} onChange={() => setChecked(!checked)} />
-      <button onClick={() => { checked ? setText('Привет пользователь!') : setText('Пока пользователь!') }}>Click</button>
-      <p>{text}</p>
+      <p>Прошу тебя пользователь, выбери с помощью чекбоксов, языки, которые ты знаешь.</p>
+      <label>
+        html
+        <input type='checkbox' checked={checked1} onChange={() => setChecked1(!checked1)} />
+      </label>
+      <label>
+        css
+        <input type='checkbox' checked={checked2} onChange={() => setChecked2(!checked2)} />
+      </label>
+      <label>
+        js
+        <input type='checkbox' checked={checked3} onChange={() => setChecked3(!checked3)} />
+      </label>
+      <p>{checked1 && 'Пользователь знает HTML'}</p>
+      <p>{checked2 && 'Пользователь знает CSS'}</p>
+      <p>{checked3 && 'Пользователь знает JS'}</p>
     </>
   )
-  
+
 }
 
 export default App;
