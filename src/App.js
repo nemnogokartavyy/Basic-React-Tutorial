5a276178e71b9518191a7343830b0e535901f3bf
nemnogokartavyy <iikorzun@yandex.by>
2025-04-17 01:00:19 +0300

    React Prime 68/112 chapter 1 task 68 gtask

--- src/App.js
+++ src/App.js
@@ -1,27 +1,23 @@
-// ⊗jsrtPmFmsSV
+// ⊗jsrtPmFmsRd
 
-import { nanoid } from 'nanoid';
 import React, { useState } from 'react';
 
 //1
 function App() {
+
   const [value, setValue] = useState('1');
 
-  return <div>
-    <select value={value} onChange={(event) => setValue(event.target.value)}>
-      <option value="1">0-12 лет</option>
-      <option value="2">13-17 лет</option>
-      <option value="3">18-25 лет</option>
-      <option value="4">25+ лет</option>
+  return (
+    <>
+      <input type='radio' name='radio' value='1' checked={value === '1' ? true : false} onChange={(event) => setValue(event.target.value)} />
+      <input type='radio' name='radio' value='2' checked={value === '2' ? true : false} onChange={(event) => setValue(event.target.value)} />
+      <input type='radio' name='radio' value='3' checked={value === '3' ? true : false} onChange={(event) => setValue(event.target.value)} />
+      <p>
+        {value}
+      </p>
+    </>
+  )
 
-    </select>
-    <p>
-      {value === '1' && '0-12 лет'}
-      {value === '2' && '13-17 лет'}
-      {value === '3' && '18-25 лет'}
-      {value === '4' && '25+ лет'}
-    </p>
-  </div>;
 }
 
 export default App;
