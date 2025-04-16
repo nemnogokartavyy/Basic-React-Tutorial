5a0461824fd37059d487f1d9a129c37d3fcaa86d
nemnogokartavyy <iikorzun@yandex.by>
2025-04-17 01:09:28 +0300

    React Prime 68/112 chapter 2 task 69 gtask

--- src/App.js
+++ src/App.js
@@ -2,18 +2,31 @@
 
 import React, { useState } from 'react';
 
-//1
+//2
 function App() {
 
-  const [value, setValue] = useState('1');
+  const [value, setValue] = useState('0');
+
+  let arr = ['js', 'css', 'html'];
 
   return (
     <>
-      <input type='radio' name='radio' value='1' checked={value === '1' ? true : false} onChange={(event) => setValue(event.target.value)} />
-      <input type='radio' name='radio' value='2' checked={value === '2' ? true : false} onChange={(event) => setValue(event.target.value)} />
-      <input type='radio' name='radio' value='3' checked={value === '3' ? true : false} onChange={(event) => setValue(event.target.value)} />
+      <label> Мой любимый ЯП - это JS
+        <input type='radio' name='radio' value='0' checked={value === '0' ? true : false} onChange={(event) => setValue(event.target.value)} />
+      </label>
+      <label> Мой любимый ЯП - это CSS
+        <input type='radio' name='radio' value='1' checked={value === '1' ? true : false} onChange={(event) => setValue(event.target.value)} />
+      </label>
+      <label> Мой любимый ЯП - это HTML
+        <input type='radio' name='radio' value='2' checked={value === '2' ? true : false} onChange={(event) => setValue(event.target.value)} />
+      </label>
       <p>
-        {value}
+        {value === '0' && arr[value]}
+        {value === '1' && arr[value]}
+        {value === '2' && arr[value]}
+      </p>
+      <p>
+        {value === '0' && 'Молодец!'}
       </p>
     </>
   )
