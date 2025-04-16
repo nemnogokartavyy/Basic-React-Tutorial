a342153e372c2ffccc2a54a5ace943941923c721
nemnogokartavyy <iikorzun@yandex.by>
2025-04-17 00:47:28 +0300

    React Prime 66/112 chapter 1 task 67 gtask

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-// ⊗jsrtPmFmsSA
+// ⊗jsrtPmFmsSV
 
 import { nanoid } from 'nanoid';
 import React, { useState } from 'react';
 
 //1
 function App() {
-  let cities = ['Минск', 'Могилев', 'Брест', 'Гомель', 'Витебск', 'Гродно']
-  const [value, setValue] = useState(cities[0]);
-
-  const options = cities.map(city => {
-    let key = nanoid();
-    return <option key={key}>{city}</option>;
-  })
+  const [value, setValue] = useState('1');
 
   return <div>
     <select value={value} onChange={(event) => setValue(event.target.value)}>
-      {options}
+      <option value="1">0-12 лет</option>
+      <option value="2">13-17 лет</option>
+      <option value="3">18-25 лет</option>
+      <option value="4">25+ лет</option>
+
     </select>
     <p>
-      {value}
+      {value === '1' && '0-12 лет'}
+      {value === '2' && '13-17 лет'}
+      {value === '3' && '18-25 лет'}
+      {value === '4' && '25+ лет'}
     </p>
   </div>;
 }
