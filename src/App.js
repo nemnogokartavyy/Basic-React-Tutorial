e1020223a98b3ee99d67b9511628a10ee9346d6b
nemnogokartavyy <iikorzun@yandex.by>
2025-04-16 23:16:35 +0300

    React Prime 63/112 chapter 2 task 64 gtask

--- src/App.js
+++ src/App.js
@@ -2,24 +2,15 @@
 
 import React, { useState } from 'react';
 
-//1
+//2
 function App() {
   const [checked, setChecked] = useState(true);
 
   let text;
   if (checked) {
-    text = <div>
-      <h2>Ура, вам уже есть 18</h2>
-      <p>
-        здесь расположен контент только для взрослых
-      </p>
-    </div>;
+    text = <p>text</p>
   } else {
-    text = <div>
-      <p>
-        увы, вам еще нет 18 лет:(
-      </p>
-    </div>;
+    text = '';
   }
 
   return <div>
