d80ef5824fd47827f631528025f219ce42f92e24
nemnogokartavyy <iikorzun@yandex.by>
2025-04-16 18:18:24 +0300

    React Prime 60/112 chapter 5 task 58 gtask

--- src/App.js
+++ src/App.js
@@ -2,25 +2,35 @@
 
 import React, { useState } from 'react';
 
-//4
+//5
 function App() {
 
   const [input, setInput] = useState(0);
   const [result, setResult] = useState(0);
 
-  function sum(number) {
-    setResult(number.split('').reduce((acc, elem) => acc += + elem, 0));
+  function mulDivisors(number) {
+    setResult(getDivisors(number).reduce((acc, elem) => acc *= elem, 1));
+  }
+
+  function getDivisors(number) {
+    let arrDivisors = [];
+    for (let i = 1; i <= number; i++) {
+      if (number % i === 0) {
+        arrDivisors.push(i);
+      }
+    }
+    return arrDivisors;
   }
 
   return (
     <>
       <input type='number' value={input}
         onChange={(event) => { setInput(event.target.value) }}
-        onBlur={() => sum(input)}
+        onBlur={() => mulDivisors(input)}
       />
 
       <p>
-        Сумма цифр числа: {result}.
+        Произведение делителей введенного числа: {result}.
       </p>
     </>
   )
