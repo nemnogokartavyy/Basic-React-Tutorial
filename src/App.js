24e980893044a19d64f32e5cc8590ce5b5f2535c
nemnogokartavyy <iikorzun@yandex.by>
2025-04-16 15:56:14 +0300

    React Prime 56/112 chapter 1 task 49 gtask

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
-// ⊗jsrtPmStIC
+// ⊗jsrtPmFmsII
 
 import React, { useState } from 'react';
 
 //1
 function App() {
 
-  const [age, setAge] = useState(30);
+  const [input1, setInput1] = useState('');
+  const [input2, setInput2] = useState('');
 
-  return <div>
-    <button onClick={() => setAge(age + 1)}>+</button>
-    <button onClick={() => setAge(age - 1)}>-</button>
-    <p>Возроаст - {age} лет.</p>
-  </div>
+  return (
+    <>
+      <input value={input1} onChange={event => setInput1(event.target.value)} />
+      <input value={input2} onChange={event => setInput2(event.target.value)} />
+      <p>{input1}</p>
+      <p>{input2}</p>
+    </>
+  )
 
 }
 
