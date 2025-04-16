e459c8d0b2443c5d96045dd0c6ff370b0c93cc57
nemnogokartavyy <iikorzun@yandex.by>
2025-04-16 14:26:35 +0300

    React Prime 53/112 chapter 1 task 45 gtask

--- src/App.js
+++ src/App.js
@@ -2,18 +2,18 @@ import uuid from 'react-uuid';
 import { nanoid } from 'nanoid'
 import React, { useState } from 'react';
 
-// ⊗jsrtPmStUs
+// ⊗jsrtPmStRt
 //1
 function App() {
 
   const [name, setName] = useState('Петя');
   const [surname, setSurname] = useState('Иванов');
-  const [age, setAge] = useState('30');
 
   return <div>
     <p>Имя: {name}</p>
     <p>Фамилия: {surname}</p>
-    <p>Возраст: {age}</p>
+    <button onClick={() => setName('Егор')}>change name</button>
+    <button onClick={() => setSurname('Петров')}>change seurname</button>
   </div>
 
 }
