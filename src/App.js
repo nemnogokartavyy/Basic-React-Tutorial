db704d40e981db05b18b4d1b12a6f8817cac6038
nemnogokartavyy <iikorzun@yandex.by>
2025-04-16 19:02:59 +0300

    React Prime 61/112 chapter 1 task 59 gtask

--- src/App.js
+++ src/App.js
@@ -1,36 +1,47 @@
-// ⊗jsrtPmFmsDt
+// ⊗jsrtPmFmsTx
 
 import React, { useState } from 'react';
 
-//5
+//1
 function App() {
 
-  const [input, setInput] = useState(0);
-  const [result, setResult] = useState(0);
+  const [value, setValue] = useState('');
+  const [res, setRes] = useState();
 
-  function mulDivisors(number) {
-    setResult(getDivisors(number).reduce((acc, elem) => acc *= elem, 1));
-  }
-
-  function getDivisors(number) {
-    let arrDivisors = [];
-    for (let i = 1; i <= number; i++) {
-      if (number % i === 0) {
-        arrDivisors.push(i);
+  function translit(text) {
+    let answer = '';
+    let converter = {
+      'а': 'a', 'б': 'b', 'в': 'v', 'г': 'g', 'д': 'd',
+      'е': 'e', 'ё': 'e', 'ж': 'zh', 'з': 'z', 'и': 'i',
+      'й': 'y', 'к': 'k', 'л': 'l', 'м': 'm', 'н': 'n',
+      'о': 'o', 'п': 'p', 'р': 'r', 'с': 's', 'т': 't',
+      'у': 'u', 'ф': 'f', 'х': 'h', 'ц': 'c', 'ч': 'ch',
+      'ш': 'sh', 'щ': 'sch', 'ь': '', 'ы': 'y', 'ъ': '',
+      'э': 'e', 'ю': 'yu', 'я': 'ya',
+      'А': 'A', 'Б': 'B', 'В': 'V', 'Г': 'G', 'Д': 'D',
+      'Е': 'E', 'Ё': 'E', 'Ж': 'Zh', 'З': 'Z', 'И': 'I',
+      'Й': 'Y', 'К': 'K', 'Л': 'L', 'М': 'M', 'Н': 'N',
+      'О': 'O', 'П': 'P', 'Р': 'R', 'С': 'S', 'Т': 'T',
+      'У': 'U', 'Ф': 'F', 'Х': 'H', 'Ц': 'C', 'Ч': 'Ch',
+      'Ш': 'Sh', 'Щ': 'Sch', 'Ь': '', 'Ы': 'Y', 'Ъ': '',
+      'Э': 'E', 'Ю': 'Yu', 'Я': 'Ya'
+    };
+    for (let i = 0; i < text.length; i++) {
+      if (converter[text[i]] == undefined) {
+        answer += text[i];
+      } else {
+        answer += converter[text[i]];
       }
     }
-    return arrDivisors;
+    return answer;
   }
 
   return (
     <>
-      <input type='number' value={input}
-        onChange={(event) => { setInput(event.target.value) }}
-        onBlur={() => mulDivisors(input)}
-      />
+      <textarea value={value} onChange={(event) => { setRes(translit(event.target.value)); setValue(event.target.value); }} />
 
       <p>
-        Произведение делителей введенного числа: {result}.
+        {res}
       </p>
     </>
   )
