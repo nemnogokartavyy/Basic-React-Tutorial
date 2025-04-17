a50dd13ed4d81a438ff4db06ee14dfbb0f5ddd15
nemnogokartavyy <iikorzun@yandex.by>
2025-04-17 23:48:54 +0300

    React Prime 75/112 chapter 1-2 tasks 80-81 gtasks

--- src/App.js
+++ src/App.js
@@ -1,85 +1,67 @@
-// ⊗jsrtPmDtAOp
+// ⊗jsrtPmDtOAA
 
 import React, { useState } from 'react';
 import { nanoid } from 'nanoid';
 
 //1
 //2
-//3
-//4
-//5
 const App = () => {
 
-    let arr = [1, 2, 3, 4, 5];
+    const initNotes = [
+        {
+            id: 'GYi9G_uC4gBF1e2SixDvu',
+            prop1: 'value11',
+            prop2: 'value12',
+            prop3: 'value13',
+        },
+        {
+            id: 'IWSpfBPSV3SXgRF87uO74',
+            prop1: 'value21',
+            prop2: 'value22',
+            prop3: 'value23',
+        },
+        {
+            id: 'JAmjRlfQT8rLTm5tG2m1L',
+            prop1: 'value31',
+            prop2: 'value32',
+            prop3: 'value33',
+        },
+    ];
 
-    const [array, setArray] = useState(arr);
-    const [input, setInput] = useState();
-    const [editIndex, setEditIndex] = useState();
+    const [notes, setNotes] = useState(initNotes);
+    const [valueProp1, setValueProp1] = useState();
+    const [valueProp2, setValueProp2] = useState();
+    const [valueProp3, setValueProp3] = useState();
 
-    let result = array.map((elem, index) => {
 
-        return <>
+    const result = notes.map(note => {
+        return <li key={note.id}>
+            <span>{note.prop1}</span>
+            <span>{note.prop2}</span>
+            <span>{note.prop3}</span>
+        </li>;
+    });
 
-            <li key={id()} onClick={() => {
-                sq(index);
-                takeInInput(elem, index);
-            }}>{elem}</li>
-
-            <button key={id()} onClick={() => del(index)}> Click</button >
-
-        </>
-
-    })
-
-    function id() {
-        let id = nanoid();
-        return id;
-    }
-
-    function sq(index) {
-        let copy = [...array];
-        copy[index] = copy[index] ** 2;
-        setArray(copy);
-    }
-
-    function del(index) {
-        let copy = [...array.slice(0, index), ...array.slice(index + 1)];
-        setArray(copy);
-        if (index === editIndex) {
-            setEditIndex(null);
-            setInput('');
+    function addElInObj() {
+        let newObj = {
+            id: nanoid(),
+            prop1: valueProp1,
+            prop2: valueProp2,
+            prop3: valueProp3,
         }
+        let copy = [...notes, newObj];
+        setNotes(copy);
     }
 
-    function takeInInput(elem, index) {
-        setEditIndex(index);
-        setInput(elem);
-    }
-
-    function takeInLi(value) {
-        let copy = [...array];
-        copy[editIndex] = value;
-        setArray(copy);
-    }
-
-    function reverseArr() {
-        let copy = [...array].reverse();
-        setArray(copy);
-    }
-
-    return (
-        <div>
-            <ul>
-                {result}
-            </ul>
-            <input value={input}
-                onChange={(event) => setInput(event.target.value)}
-                onBlur={(event) => takeInLi(event.target.value)} />
-            <div>
-                <button onClick={() => reverseArr()}>Reverse</button>
-            </div>
-        </div>
-    )
+    return <div>
+        <ul>
+            {result}
+        </ul>
+        <input value={valueProp1} onChange={(event) => setValueProp1(event.target.value)} />
+        <input value={valueProp2} onChange={(event) => setValueProp2(event.target.value)} />
+        <input value={valueProp3} onChange={(event) => setValueProp3(event.target.value)} />
+        <button onClick={() => addElInObj()}>Click</button>
+    </div>;
 
 };
 
