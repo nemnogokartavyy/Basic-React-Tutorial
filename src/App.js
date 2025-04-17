d47c72405dd298a777688dad7fd460fab70faf14
nemnogokartavyy <iikorzun@yandex.by>
2025-04-17 17:57:48 +0300

    React Prime 71/112 chapter 1-2 tasks 71-72 gtasks

--- src/App.js
+++ src/App.js
@@ -1,38 +1,101 @@
-// ⊗jsrtPmFmsAIB
+// ⊗jsrtPmFmsOIB
 
 import React, { useState } from 'react';
 
 //1
-function App() {
+//2
+const App = () => {
 
-  let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-  const [notes, setNotes] = useState(arr);
-  let sum = 0;
+    const initDate = {
+        year: 2025,
+        month: 12,
+        day: 31,
+    }
 
-  const inputs = notes.map((note, index) => {
-    return <input
-      key={index}
-      value={note}
-      onChange={event => change(index, event)}
-    />;
-  });
+    const [date, setDate] = useState(initDate);
 
-  function change(index, event) {
-    setNotes([...notes.slice(0, index), event.target.value, ...notes.slice(index + 1)])
-  }
+    function handleChange(event, name) {
+        const nValue = Number(event.target.value);
 
-  function averageArithmetic(arr) {
-    sum = arr.reduce((acc, elem) => acc += +elem, 0) / arr.length;
-    return sum;
-  }
+        const copy = Object.assign({}, date);
+        copy[name] = nValue;
+        setDate(copy);
+    };
 
-  return (
-    <>
-      {inputs};
-      <p>{averageArithmetic(notes)}</p>
-    </>
-  )
+    const isValidDate =
+        date.year > 0 &&
+        date.month >= 1 &&
+        date.month <= 12 &&
+        date.day >= 1 &&
+        date.day <= 31;
 
-}
+    let dayOfWeek = '';
+
+    if (isValidDate) {
+
+        const resultDate = new Date(date.year, date.month - 1, date.day);
+
+        if (resultDate.getFullYear() === date.year
+            && resultDate.getMonth() === date.month - 1
+            && resultDate.getDate() === date.day) {
+
+            const arr = [
+                'вс',
+                'пн',
+                'вт',
+                'ср',
+                'чт',
+                'пт',
+                'сб',
+            ];
+
+            dayOfWeek = arr[resultDate.getDay()];
+
+        } else {
+            dayOfWeek = 'Дата некорректная';
+        }
+        
+    } else {
+        dayOfWeek = 'Дата некорректная';
+    }
+
+    return (
+        <div>
+
+            <input
+                name="yea
+                type='number'r"
+                value={date.year}
+                onChange={(event) => handleChange(event, 'year')}
+                min="1"
+            />
+
+            <input
+                type='number'
+                name="month"
+                value={date.month}
+                onChange={(event) => handleChange(event, 'month')}
+                min="1"
+                max="12"
+            />
+
+            <input
+                type='number'
+                name="day"
+                value={date.day}
+                onChange={(event) => handleChange(event, 'day')}
+                min="1"
+                max="31"
+            />
+
+
+            <p>
+                {date.year}-{date.month}-{date.day}-{dayOfWeek}
+            </p>
+
+        </div>
+    );
+
+};
 
 export default App;
