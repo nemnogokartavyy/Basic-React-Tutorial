e5c317af726e6a1c6db4c14dd7582401a130e47e
nemnogokartavyy <iikorzun@yandex.by>
2025-04-17 23:37:00 +0300

    React Prime 74/112 chapter 1-5 tasks 75-79 gtasks

--- src/App.js
+++ src/App.js
@@ -1,37 +1,86 @@
-// ⊗jsrtPmDtAA
+// ⊗jsrtPmDtAOp
 
 import React, { useState } from 'react';
+import { nanoid } from 'nanoid';
 
 //1
 //2
+//3
+//4
+//5
 const App = () => {
 
     let arr = [1, 2, 3, 4, 5];
 
     const [array, setArray] = useState(arr);
     const [input, setInput] = useState();
+    const [editIndex, setEditIndex] = useState();
 
     let result = array.map((elem, index) => {
-        return <li key={index}>{elem}</li>
-    });
 
-    function func() {
+        return <>
+
+            <li key={id()} onClick={() => {
+                sq(index);
+                takeInInput(elem, index);
+            }}>{elem}</li>
+
+            <button key={id()} onClick={() => del(index)}> Click</button >
+
+        </>
+
+    })
+
+    function id() {
+        let id = nanoid();
+        return id;
+    }
+
+    function sq(index) {
         let copy = [...array];
-        copy.push(+input);
+        copy[index] = copy[index] ** 2;
         setArray(copy);
-        setInput('');
-    };
+    }
+
+    function del(index) {
+        let copy = [...array.slice(0, index), ...array.slice(index + 1)];
+        setArray(copy);
+        if (index === editIndex) {
+            setEditIndex(null);
+            setInput('');
+        }
+    }
+
+    function takeInInput(elem, index) {
+        setEditIndex(index);
+        setInput(elem);
+    }
+
+    function takeInLi(value) {
+        let copy = [...array];
+        copy[editIndex] = value;
+        setArray(copy);
+    }
+
+    function reverseArr() {
+        let copy = [...array].reverse();
+        setArray(copy);
+    }
 
     return (
-        <>
+        <div>
             <ul>
                 {result}
             </ul>
-            <input value={input} onChange={event => setInput(event.target.value)} />
-            <button onClick={() => func()}>Click</button>
-        </>
+            <input value={input}
+                onChange={(event) => setInput(event.target.value)}
+                onBlur={(event) => takeInLi(event.target.value)} />
+            <div>
+                <button onClick={() => reverseArr()}>Reverse</button>
+            </div>
+        </div>
     )
-    
+
 };
 
 export default App;
