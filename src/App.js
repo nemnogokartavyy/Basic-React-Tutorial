a0787b00963d4cb601567dd577f4f52c87297f86
nemnogokartavyy <iikorzun@yandex.by>
2025-04-18 20:12:40 +0300

    React Prime 79/112 chapter 1 task 86 gtask

--- src/App.js
+++ src/App.js
@@ -1,63 +1,11 @@
-// ⊗jsrtPmDtSh
+// ⊗jsrtPmCpUs
 
 //1
 import React, { useState } from 'react';
+import { User } from './components/User';
 
 const App = () => {
-
-    const initNotes = [
-        {
-            id: 1,
-            name: 'name1',
-            desc: 'long description 1',
-            show: false,
-        },
-        {
-            id: 2,
-            name: 'name2',
-            desc: 'long description 2',
-            show: false,
-        },
-        {
-            id: 3,
-            name: 'name3',
-            desc: 'long description 3',
-            show: false,
-        },
-    ];
-
-    const [notes, setNotes] = useState(initNotes);
-
-    const result = notes.map(note => {
-        return <>
-            {note.show
-                ? <p key={note.id}>
-                    {note.name},
-                    <i>{note.desc}</i>
-                </p>
-                : <p key={note.id}>
-                    {note.name},
-                </p>
-            }
-            <button onClick={() => showElem(note.id)}>Show Descr</button>
-        </>
-    });
-
-    function showElem(id) {
-        let copy = Object.assign(notes);
-        copy = notes.map(elem => {
-            if (elem.id === id) {
-                return { ...elem, show: !elem.show }
-            }
-            return elem;
-        })
-        setNotes(copy);
-    }
-
-    return <div>
-        {result}
-    </div>;
-
+    return <User />
 };
 
 export default App;
