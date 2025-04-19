8b165aba0fbdc2cb0378adaba45744120134206c
nemnogokartavyy <iikorzun@yandex.by>
2025-04-19 15:50:40 +0300

    React Prime 96/112 chapter 1 task 101 gtask

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
-// ⊗jsrtPmCoLSU
+// ⊗jsrtPmCoTOS
 
 //1
-//2
-import React from 'react';
-
-import Calculator from './temperature/Calculator';
-import BMICalculator from './bodymassindex/BMICalculator';
+import React, { useState } from 'react';
 
 const App = () => {
 
+    const [temp, setTemp] = useState(0);
+
     return (
-        <>
-            <Calculator />
-            <BMICalculator />
-        </>
+        <div>
+            <label>
+                Температура по цельсию: <input type='number'
+                    value={temp} onChange={(event) => setTemp(event.target.value)} />
+            </label>
+            <label>
+                Температура по фарегейту: <input type='number'
+                    value={((temp * 9 / 5) + 32)} onChange={(event) => setTemp((event.target.value - 32) * 5 / 9)} />
+            </label>
+        </div>
     )
+
 };
 
 export default App;
