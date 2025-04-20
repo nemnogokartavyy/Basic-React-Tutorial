0209fb6d43e797fc410e5be5aa0c1b55133cc339
nemnogokartavyy <iikorzun@yandex.by>
2025-04-20 22:59:22 +0300

    React Prime 101/112 chapter 1 task 105 gtask

--- src/App.js
+++ src/App.js
@@ -1,18 +1,43 @@
-// ⊗jsrtPmSyCFTS
+// ⊗jsrtPmSySIS
 
 //1
 import React, { useState } from 'react';
 import './styles.css';
-
 import { styles } from './styles';
+
 const App = () => {
 
     return (
 
         <>
-            <div style={styles.block}>
-                <button style={styles.btn1}>Click1</button>
-                <button style={styles.btn2}>Click1</button>
+            <div style={{
+                display: 'flex',
+                justifyContent: 'center',
+                padding: '25px',
+                alignItems: 'center',
+                backgroundColor: '#0099ff',
+            }}>
+                <button style={{
+                    marginRight: '25px',
+                    backgroundColor: '#ffffff',
+                    border: '2px solid #2e4eb6',
+                    padding: '10px 25px',
+                    borderRadius: '10px',
+                    fontFamily: 'Verdana, Geneva, Tahoma, sans-serif',
+                    fontWeight: 'bold',
+                    color: '#2e4eb6',
+                    cursor: 'pointer',
+                }}>Click1</button>
+                <button style={{
+                    backgroundColor: '#ffffff',
+                    border: '2px solid #2e4eb6',
+                    padding: '10px 25px',
+                    borderRadius: '10px',
+                    fontFamily: 'Verdana, Geneva, Tahoma, sans-serif',
+                    fontWeight: 'bold',
+                    color: '#2e4eb6',
+                    cursor: 'pointer',
+                }}>Click1</button>
             </div>
         </>
 
