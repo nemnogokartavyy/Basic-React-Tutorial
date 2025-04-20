a0fc82b472d85fd973eda4a9d56d44053dfe8549
nemnogokartavyy <iikorzun@yandex.by>
2025-04-20 22:56:15 +0300

    React Prime 100/112 chapter 1 task 104 gtask

---
+++ src/styles.js
@@ -0,0 +1,36 @@
+const block = {
+    display: 'flex',
+    justifyContent: 'center',
+    padding: '25px',
+    alignItems: 'center',
+    backgroundColor: '#0099ff',
+}
+
+const btn1 = {
+    marginRight: '25px',
+    backgroundColor: '#ffffff',
+    border: '2px solid #2e4eb6',
+    padding: '10px 25px',
+    borderRadius: '10px',
+    fontFamily: 'Verdana, Geneva, Tahoma, sans-serif',
+    fontWeight: 'bold',
+    color: '#2e4eb6',
+    cursor: 'pointer',
+}
+
+const btn2 = {
+    backgroundColor: '#ffffff',
+    border: '2px solid #2e4eb6',
+    padding: '10px 25px',
+    borderRadius: '10px',
+    fontFamily: 'Verdana, Geneva, Tahoma, sans-serif',
+    fontWeight: 'bold',
+    color: '#2e4eb6',
+    cursor: 'pointer',
+}
+
+export const styles = {
+    block: block,
+    btn1: btn1,
+    btn2: btn2,
+}
