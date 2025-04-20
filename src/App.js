67d0b5cd5594b06a92e0cf9ee531da50b248deec
nemnogokartavyy <iikorzun@yandex.by>
2025-04-20 22:51:20 +0300

    React Prime 99/112 chapter 1 task 103 gtask

--- src/App.js
+++ src/App.js
@@ -1,17 +1,47 @@
-// ⊗jsrtPmSyGlC
+// ⊗jsrtPmSyOTS
 
 //1
 import React, { useState } from 'react';
 import './styles.css';
-
 const App = () => {
+
+    const block = {
+        display: 'flex',
+        justifyContent: 'center',
+        padding: '25px',
+        alignItems: 'center',
+        backgroundColor: '#0099ff',
+    }
+
+    const btn1 = {
+        marginRight: '25px',
+        backgroundColor: '#ffffff',
+        border: '2px solid #2e4eb6',
+        padding: '10px 25px',
+        borderRadius: '10px',
+        fontFamily: 'Verdana, Geneva, Tahoma, sans-serif',
+        fontWeight: 'bold',
+        color: '#2e4eb6',
+        cursor: 'pointer',
+    }
+
+    const btn2 = {
+        backgroundColor: '#ffffff',
+        border: '2px solid #2e4eb6',
+        padding: '10px 25px',
+        borderRadius: '10px',
+        fontFamily: 'Verdana, Geneva, Tahoma, sans-serif',
+        fontWeight: 'bold',
+        color: '#2e4eb6',
+        cursor: 'pointer',
+    }
 
     return (
 
         <>
-            <div className='block'>
-                <button className='btn1'>Click1</button>
-                <button className='btn2'>Click1</button>
+            <div style={block}>
+                <button style={btn1}>Click1</button>
+                <button style={btn2}>Click1</button>
             </div>
         </>
 
