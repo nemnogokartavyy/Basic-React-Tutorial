780123f387004bb3c6ead114d65a5d45f614ccea
nemnogokartavyy <iikorzun@yandex.by>
2025-04-21 20:55:53 +0300

    React Prime 103/112 chapter 1 task 107 gtask

--- src/App.js
+++ src/App.js
@@ -1,61 +1,48 @@
-// ⊗jsrtPmSySIS
+// ⊗jsrtPmSyStC
 
 //1
-import React, { useState } from 'react';
-import './styles.css';
-import { styles } from './styles';
+import React from 'react';
+import styled from 'styled-components';
 
-const App = () => {
+const Block = styled.div`
+    display: flex;
+    justify-content: center;
+    padding: 25px;
+    align-items: center;
+    background-color: #0099ff;
+`;
 
-    const bDisplay = 'flex';
-    const bJustifyContent = 'center';
-    const bPadding = '25px';
-    const bAlignItems = 'center';
-    const bBackgroundColor = '#0099ff';
+const Button1 = styled.button`
+    margin-right: 25px;
+    background-color: #ffffff;
+    border: 2px solid #2e4eb6;
+    padding: 10px 25px;
+    border-radius: 10px;
+    font-family: Verdana, Geneva, Tahoma, sans-serif;
+    font-weight: bold;
+    color: #2e4eb6;
+    cursor: pointer;
+`;
 
-    const buttonMarginRight = '25px';
-    const buttonBackgroundColor = '#ffffff';
-    const buttonBorder = '2px solid #2e4eb6';
-    const buttonPadding = '10px 25px';
-    const buttonBorderRadius = '10px';
-    const buttonFontFamily = 'Verdana, Geneva, Tahoma, sans-serif';
-    const buttonFontWeight = 'bold';
-    const buttonColor = '#2e4eb6';
-    const buttonCursor = 'pointer';
+const Button2 = styled.button`
+    background-color: #ffffff;
+    border: 2px solid #2e4eb6;
+    padding: 10px 25px;
+    border-radius: 10px;
+    font-family: Verdana, Geneva, Tahoma, sans-serif;
+    font-weight: bold;
+    color: #2e4eb6;
+    cursor: pointer;
+`;
+
+function App() {
 
     return (
 
-        <>
-            <div style={{
-                display: bDisplay,
-                justifyContent: bJustifyContent,
-                padding: bPadding,
-                alignItems: bAlignItems,
-                backgroundColor: bBackgroundColor,
-            }}>
-                <button style={{
-                    marginRight: buttonMarginRight,
-                    backgroundColor: buttonBackgroundColor,
-                    border: buttonBorder,
-                    padding: buttonPadding,
-                    borderRadius: buttonBorderRadius,
-                    fontFamily: buttonFontFamily,
-                    fontWeight: buttonFontWeight,
-                    color: buttonColor,
-                    cursor: buttonCursor,
-                }}>Click1</button>
-                <button style={{
-                    backgroundColor: buttonBackgroundColor,
-                    border: buttonBorder,
-                    padding: buttonPadding,
-                    borderRadius: buttonBorderRadius,
-                    fontFamily: buttonFontFamily,
-                    fontWeight: buttonFontWeight,
-                    color: buttonColor,
-                    cursor: buttonCursor,
-                }}>Click1</button>
-            </div>
-        </>
+        <Block>
+            <Button1>Click1</Button1>
+            <Button2>Click1</Button2>
+        </Block>
 
     );
 
